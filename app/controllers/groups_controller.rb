e6c3f6d64b502284262a645b2f0a4f3bd8c77dfd
wangzhuoyang <heu07051126@gmail.com>
2017-02-24 23:22:48 +0800

    use before_action to find_group_and_check_permission errot

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,9 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create，:edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
+   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
+
   def index
     @groups = Group.all
   end
